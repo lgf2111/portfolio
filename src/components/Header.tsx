a0948d49c6ecc1a7c3861f3fc4e02456987e1cd1
lgf2111 <lgf2111@gmail.com>
2023-05-15 01:40:16 +0800

    home page done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,57 +2,55 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <section>
-      <header className="container mx-auto mt-5 px-5 flex justify-between">
-        <div>
-          <h1 className="text-xl text-green-400">Lee Guan Feng</h1>
-        </div>
-        <div>
-          <ul className="flex gap-x-5 text-green-600">
-            <li>
-              <Link
-                to="/"
-                className="group transition-all duration-300 ease-in-out"
-              >
-                <span className="bg-left-bottom bg-gradient-to-r from-green-600 to-green-400 bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_1px] group-hover:text-green-400 transition-all duration-500 ease-out">
-                  Home
-                </span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/about"
-                className="group transition-all duration-300 ease-in-out"
-              >
-                <span className="bg-left-bottom bg-gradient-to-r from-green-600 to-green-400 bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_1px] group-hover:text-green-400 transition-all duration-500 ease-out">
-                  About
-                </span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/projects"
-                className="group transition-all duration-300 ease-in-out"
-              >
-                <span className="bg-left-bottom bg-gradient-to-r from-green-600 to-green-400 bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_1px] group-hover:text-green-400 transition-all duration-500 ease-out">
-                  Projects
-                </span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/contact"
-                className="group transition-all duration-300 ease-in-out"
-              >
-                <span className="bg-left-bottom bg-gradient-to-r from-green-600 to-green-400 bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_1px] group-hover:text-green-400 transition-all duration-500 ease-out">
-                  Contact
-                </span>
-              </Link>
-            </li>
-          </ul>{" "}
-        </div>
-      </header>
-    </section>
+    <header className="container mx-auto mt-5 px-5 flex justify-between">
+      <div>
+        <h1 className="text-xl text-green-400">Lee Guan Feng</h1>
+      </div>
+      <div>
+        <ul className="flex gap-x-5 text-green-600">
+          <li>
+            <Link
+              to="/"
+              className="group transition-all duration-300 ease-in-out"
+            >
+              <span className="bg-left-bottom bg-gradient-to-r from-green-600 to-green-400 bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_1px] group-hover:text-green-400 transition-all duration-500 ease-out">
+                Home
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className="group transition-all duration-300 ease-in-out"
+            >
+              <span className="bg-left-bottom bg-gradient-to-r from-green-600 to-green-400 bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_1px] group-hover:text-green-400 transition-all duration-500 ease-out">
+                About
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/projects"
+              className="group transition-all duration-300 ease-in-out"
+            >
+              <span className="bg-left-bottom bg-gradient-to-r from-green-600 to-green-400 bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_1px] group-hover:text-green-400 transition-all duration-500 ease-out">
+                Projects
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className="group transition-all duration-300 ease-in-out"
+            >
+              <span className="bg-left-bottom bg-gradient-to-r from-green-600 to-green-400 bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_1px] group-hover:text-green-400 transition-all duration-500 ease-out">
+                Contact
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,29 @@
+import React from "react";
 import Layout from "../components/Layout";
 
 const Home = () => {
   return (
     <Layout>
-      <h1>Home</h1>
+      <div className="flex my-40">
+        <div className="w-1/2 flex flex-col justify-center items-end pr-5 text-right">
+          <h1 className="text-2xl text-green-400">Hi, I'm Guan Feng!</h1>
+          <p className="text-gray-100 w-3/5">
+            I am from <b>Singapore🇸🇬</b> and I love <b>coding👨🏻‍💻</b>. My passion
+            is creating beautiful and functional websites and web applications
+            using various programming languages such as <b>HTML</b>, <b>CSS</b>,
+            <b>JavaScript</b>, and <b>Python</b>. I strive to stay up-to-date
+            with the latest industry trends and always focus on writing clean
+            and efficient code. Let's build something amazing together!
+          </p>
+        </div>
+        <div className="w-1/2 pl-5">
+          <img
+            src={require("../assets/images/landing.jpg")}
+            alt="Image"
+            className="w-80 h-80 object-cover rounded-lg"
+          />
+        </div>
+      </div>
     </Layout>
   );
 };
